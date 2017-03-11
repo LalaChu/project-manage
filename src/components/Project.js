0ae44aa8c7aae6ba1b9ee961587f95a38aa3707a
hquiet <agjr@qq.com>
2017-03-11 19:06:00 +0800

    add add item button

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react'
 import Table from 'antd/lib/table'
 import Button from 'antd/lib/button'
 import columns from '../tableStructure/project'
+import { addKeyColumns } from '../helper'
+import Icon from 'antd/lib/icon'
 
 class Project extends Component{
     componentWillMount(){
         this.props.getProjectList()
     }
     render(){
-        console.log('project:',this.props)
+        const dataSource = addKeyColumns(this.props.list)
+        console.log(dataSource)
         return (
             <div className='project-list'>
-                <Button></Button>
+                <Button className='add-item'><Icon type="plus" /></Button>
                 <Table 
+                    size='middle'
+                    dataSource={dataSource}
                     columns={columns} 
                     bordered/>
             </div>
