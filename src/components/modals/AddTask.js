a70729abc684192674f2d4b8788926a85d963270
hquiet <agjr@qq.com>
2017-03-12 12:47:22 +0800

    complete the framework of the form when add project,category or task

--- src/components/modals/AddTask.js
+++ src/components/modals/AddTask.js
@@ -1,15 +1,69 @@
 import React, { Component } from 'react'
 import Modal from 'antd/lib/modal'
+import Form from 'antd/lib/form'
+import Input from 'antd/lib/input'
+
+const FormItem = Form.Item
 
 class AddTask extends Component{
     handleCancel = () => {
         this.props.onCancel(false)
     }
     render(){
+        const formLayout = {
+            labelCol: {span : 6},
+            wrapperCol: {span: 14}
+        }
         return (
             <Modal
                 onCancel={this.handleCancel}
-                visible={this.props.visible}>AddTask</Modal>
+                visible={this.props.visible}
+                title='添加具体工作'>
+                <Form>
+                    <FormItem 
+                        {...formLayout}
+                        label='名称'
+                        hasFeedback>
+                        <Input />
+                    </FormItem>
+                    <FormItem 
+                        {...formLayout}
+                        label='父节点'
+                        hasFeedback>
+                        <Input />
+                    </FormItem>
+                    <FormItem 
+                        {...formLayout}
+                        label='负责人'
+                        hasFeedback>
+                        <Input />
+                    </FormItem>
+                    <FormItem 
+                        {...formLayout}
+                        label='开始时间'
+                        hasFeedback>
+                        <Input />
+                    </FormItem>
+                    <FormItem 
+                        {...formLayout}
+                        label='结束时间'
+                        hasFeedback>
+                        <Input />
+                    </FormItem>
+                    <FormItem 
+                        {...formLayout}
+                        label='类型'
+                        hasFeedback>
+                        <Input />
+                    </FormItem>
+                    <FormItem 
+                        {...formLayout}
+                        label='描述'
+                        hasFeedback>
+                        <Input />
+                    </FormItem>
+                </Form>
+            </Modal>
         )
     }
 }
