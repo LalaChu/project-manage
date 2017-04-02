2d0494517d4740695490fc4e577531166bb1117c
hquiet <agjr@qq.com>
2017-04-02 18:40:20 +0800

    add the constants of daily

---
+++ src/constants/daily.js
@@ -0,0 +1,5 @@
+export const ADD_DAILY = 'ADD_DAILY'
+export const EDIT_DAILY = 'EDIT_DAILY'
+export const DELETE_DAILY = 'DELETE_DAILY'
+export const VIEW_DAILY = 'VIEW_DAILY'
+export const POST_DAILY_FILE = 'POST_DAILY_FILE'
